81e541a424b532f790b20ebe5c0dad6128414672
Moses Gonzalez <mgonzalez1130@gmail.com>
2014-10-10 23:41:12 -0700

    hexdump working
    Got the process builder to execute the command: xxd -l 0x36
    wayfaring.wav

--- assignment5/TestingProcessBuilder/TestAssignment5.java
+++ assignment5/TestingProcessBuilder/TestAssignment5.java
@@ -8,143 +8,106 @@ import java.util.*;
 
 public class TestAssignment5 {
 
-	
+
 	public static void main(String[] args) {
-		
+
 		//create ArrayList for command: pushd /course/cs5500f14/Examples/FFT
 		List<String> command1 = new ArrayList<String>();
-		command1.add("pushd");
-		command1.add("/course/cs5500f14/Examples/FFT");
-		
-		//create ArrayList for command: larceny --err5rs
-		List<String> command2 = new ArrayList<String>();
-		command2.add("larceny");
-		command2.add("--err5rs");
-		
-		//create ArrayList for command: (import (rnrs) (larceny load))
-		List<String> command3 = new ArrayList<String>();
-		command3.add("import");
-		command3.add("(rnrs)");
-		command3.add("(larceny");
-		command3.add("load))");
-		
-		//create ArrayList for command: (load "demo3.sch")
-		List<String> command4 = new ArrayList<String>();
-		command4.add("(load");
-		command4.add("\"demo2.sch\")");
-		
-		//create ArrayList for command: (import (local fft) (local aux))
-		List<String> command5 = new ArrayList<String>();
-		command5.add("(import");
-		command5.add("(local");
-		command5.add("fft)");
-		command5.add("(local");
-		command5.add("aux))");
-		
-		//create ArrayList for command: (vector-map magnitude (fft v3))
-		List<String> command6 = new ArrayList<String>();
-		command6.add("vector-map");
-		command6.add("magnitude");
-		command6.add("(fft");
-		command6.add("v3))");
-		
+		command1.add("xxd");
+		command1.add("-l");
+		command1.add("0x36");
+		command1.add("wayfaring.wav");
+
 		executeCommands(command1);
-		executeCommands(command2);
-		executeCommands(command3);
-		executeCommands(command4);
-		executeCommands(command5);
-		executeCommands(command6);
 	}
 
+
 	private static void executeCommands(List<String> command) {
-		//Initalize ProcessBuilder
+		//Initialize ProcessBuilder
 		ProcessBuilder pb = new ProcessBuilder(command);
-		
+
 		//Start Process
 		try {
 			Process process = pb.start();
 
-		
-		//Print output and error streams
-		 StringBuilder input = new StringBuilder();
-		 input = printInput(process.getInputStream());
-		 
-		 StringBuilder error = new StringBuilder();
-		 error = printError(process.getErrorStream());
-		 
-		 System.out.println(input);
-		 System.out.println(error);
-		 
+			OutputStream stdOutput = process.getOutputStream();
+			
+			//Print output and error streams
+			StringBuilder input = new StringBuilder();
+			input = printInput(process.getInputStream());
+
+			StringBuilder error = new StringBuilder();
+			error = printError(process.getErrorStream());
+
+			System.out.println(input);
+			System.out.println(error);
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+	}
+
+	private static StringBuilder printError(InputStream errorStream) {
+		BufferedReader bufferedReader = null;
+		StringBuilder error = new StringBuilder();
+
+		try
+		{
+			bufferedReader = new BufferedReader(new InputStreamReader(errorStream));
+			String line = null;
+			while ((line = bufferedReader.readLine()) != null)
+			{
+				error.append(line + "\n");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		finally
+		{
+			try
+			{
+				bufferedReader.close();
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+
+		return error;
+	}
+
+	private static StringBuilder printInput(InputStream inputStream) {
+		BufferedReader bufferedReader = null;
+		StringBuilder input = new StringBuilder();
+
+		try
+		{
+			bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+			String line = null;
+			while ((line = bufferedReader.readLine()) != null)
+			{
+				input.append(line + "\n");
+			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		 
-
-		
-	}
-
-	private static StringBuilder printError(InputStream errorStream) {
-		 BufferedReader bufferedReader = null;
-		 StringBuilder error = new StringBuilder();
-		 
-		 try
-		 {
-		   bufferedReader = new BufferedReader(new InputStreamReader(errorStream));
-		   String line = null;
-		   while ((line = bufferedReader.readLine()) != null)
-		   {
-			   error.append(line + "\n");
-		   }
-		 } catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		finally
+		{
+			try
+			{
+				bufferedReader.close();
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
 		}
-		 finally
-		 {
-		   try
-		   {
-		     bufferedReader.close();
-		   }
-		   catch (IOException e)
-		   {
-		     // ignore this one
-		   }
-		 }
-		 
-		 return error;
+
+		return input;
 	}
 
-	private static StringBuilder printInput(InputStream inputStream) {
-		 BufferedReader bufferedReader = null;
-		 StringBuilder input = new StringBuilder();
-		 
-		 try
-		 {
-		   bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-		   String line = null;
-		   while ((line = bufferedReader.readLine()) != null)
-		   {
-			   input.append(line + "\n");
-		   }
-		 } catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		 finally
-		 {
-		   try
-		   {
-		     bufferedReader.close();
-		   }
-		   catch (IOException e)
-		   {
-		     // ignore this one
-		   }
-		 }
-		 
-		 return input;
-	}
-	
-	
+
 }
