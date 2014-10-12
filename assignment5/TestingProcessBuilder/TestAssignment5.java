21e7915eb44f194d541b65341db9de402430c641
Moses Gonzalez <mgonzalez1130@gmail.com>
2014-10-12 04:26:23 -0700

    Outline for Assignment5
    This is assuming we can use the Columbia java implementation of FFT
    (waiting for response)

--- assignment5/TestingProcessBuilder/TestAssignment5.java
+++ assignment5/TestingProcessBuilder/TestAssignment5.java
@@ -1,23 +1,56 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+
+import java.io.*;
 import java.util.*;
 
 
 public class TestAssignment5 {
 
+	static byte[] fileOneArray;
+	static byte[] fileTwoArray;
 
 	public static void main(String[] args) {
 
-		//create ArrayList for command: pushd /course/cs5500f14/Examples/FFT
-		List<String> command1 = new ArrayList<String>();
-		command1.add("xxd");
-		command1.add("-l");
-		command1.add("0x36");
-		command1.add("wayfaring.wav");
 
-		linuxCommand.executeCommand(command1);
+		//check file path is ok, if not return 
+		//ERROR: incorrect command line
+
+		//check file extension, if not .wav return
+		//ERROR: fileName is not a supported format
+
+		//check file is in WAVE format, if not return
+		//ERROR: File not in WAVE format
+
+		for (int i = 0; i < args.length; i++) {
+			byte[] currentFileArray;
+
+			try {
+				String filePath = args[i];
+				File file = new File(filePath);
+				InputStream inputStream = new FileInputStream(file);
+				currentFileArray = new byte[(int)file.length()];
+				inputStream.read(currentFileArray, 0, currentFileArray.length);
+
+				if (i == 0) {
+					fileOneArray = currentFileArray;
+				} else {
+					fileTwoArray = currentFileArray;
+				}
+
+				inputStream.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		//Create imaginary byte arrays of same lengths
+		//Convert to frequency domain using Columbia fft file
+		//Create frequency array
+		//Convert the real and imaginary components of results to
+		//frequencies using sqrt (real * real + img * img)
+		//Compare frequencies using mean squared error
+		//Return MATCH if error < %10, otherwise return NO MATCH
 	}
 }
+
