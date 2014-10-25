b97ec5698f09832ad7e919ffccd9aeda4c09b9a4
Zhuoli <liang.zhu@husky.neu.edu>
2014-10-25 14:09:08 -0700

    wav rewrite done

--- assignment5/src/comparator/Audio.java
+++ assignment5/src/comparator/Audio.java
@@ -4,7 +4,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.ByteBuffer;
 import java.util.Arrays;
 /*
  */
@@ -14,8 +13,9 @@ public class Audio {
 	private static boolean DEBUG=true;
 	String fileName;
 	double sampleRate;
-	String waveFormat;
+	String format;
 	int bitesPerSecond;
+	int numChannels;
 	
 	byte[] fileArray;
 	short[] leftChannelSamples;
@@ -29,17 +29,23 @@ public class Audio {
 		this.fileArray=readFile2ByteArray(file);
 		
 		byte[] header=Arrays.copyOfRange(fileArray, 0, 44);
-		if(this.checkCDSpecs(header,fileArray.length-44)!=0){
+		double[] sr=new double[1];
+		String[] wf=new String[1];
+		int[] bps  =new int[1];
+		int[] nc   =new int[1];
+		if(this.checkCDSpecs(header,fileArray.length-44,sr,wf,bps,nc)!=0){
 				System.err.println("File one does not match CD specification");
 				System.exit(1);
 		}
-		sampleRate=getSampleRate(header);
-		waveFormat=getWaveFormat(header);
-		bitesPerSecond=getBitsPerSample(header);
-	
-		final byte[] fileLeftChannel = extractLeftChannels();	
-		timeZoneData = convertToDoubles(fileLeftChannel);
-		leftChannelSamples=convertToShort(fileLeftChannel);
+		sampleRate=sr[0];
+		format=wf[0];
+		bitesPerSecond=bps[0];
+		numChannels=nc[0];
+		
+		
+		final byte[] fileLeftChannel = extractLeftChannels(this.numChannels,this.bitesPerSecond);	
+		timeZoneData = convertToDoubles(fileLeftChannel,this.bitesPerSecond);
+		leftChannelSamples=convertToShort(fileLeftChannel,this.bitesPerSecond);
 		double[] fileImg=applyFFT(timeZoneData);
 		frequenciesData=convertToFrequencies(fileImg,timeZoneData);
 	}
@@ -63,7 +69,7 @@ public class Audio {
 	public String toString(){
 		StringBuilder sb = new StringBuilder();
 		sb.append("Filename: "+this.fileName+"\n");
-		sb.append("Wave Format: "+this.waveFormat+"\n");
+		sb.append("Audio Format: "+this.format+"\n");
 		sb.append("Sample Rate: "+this.sampleRate+"\n");
 		sb.append("Bites per Second: "+this.bitesPerSecond+"\n");
 		return sb.toString();
@@ -80,16 +86,6 @@ public class Audio {
 	}
 	public double getSampleRate(){
 		return this.sampleRate;
-	}
-	/** Private Methods 		**/
-	private double getSampleRate(byte[] bytes){
-		return 44.1;
-	}
-	public String getWaveFormat(byte[] bytes){
-		return "PCM";
-	}
-	public int getBitsPerSample(byte[] bytes){
-		return 16;
 	}
 	// read file to array
 	private byte[] readFile2ByteArray(File file){
@@ -109,35 +105,44 @@ public class Audio {
 		} 
 		return fileArray;
 	}
-	private int checkCDSpecs(byte[] bytes,int datasize) {
+	// check audio file headline format
+	private int checkCDSpecs(byte[] bytes,int datasize,double[] sampleRate ,String[] format, int[] bps,int[] nc) {
 			//Check format = wave
 			if (bytes[8] != 87 || bytes[9] != 65 ||
 					bytes[10] != 86 || bytes[11] != 69) {
-				if (DEBUG) { System.out.println("Wave Format"); }
+				if (DEBUG) { System.err.println("ERROR: NOT Wave Format"); }
 				return -1;
 				
 			}
+			format[0]="wav";
 			//Check audio format = PCM
 			if (bytes[20] != 1 || bytes[21] != 0) {
-				if (DEBUG) { System.out.println("PCM"); }
+				if (DEBUG) { System.err.println("ERROR: NOT PCM"); }
 				return -1;
 			}
-			//Check channels = stereo
-			if (bytes[22] != 2 || bytes[23] != 0) {
-				if (DEBUG) {System.out.println("Stereo Channels"); }
+			//Check channels = stereo or Mono
+			if (bytes[22] == 2 && bytes[23] == 0) {
+				nc[0]=2;
+			}else if(bytes[22]==1 && bytes[23]==0){
+				nc[0]=1;
+			}else{
+				if(DEBUG){System.err.println("ERROR: Incorrect num of channels");}
 				return -1;
 			}
 			//Check sample rate is 44.1 kHz
-			if (bytes[24] != 68 || bytes[25] != -84
-					|| bytes[26] != 0 || bytes[27] != 0) {
-				if (DEBUG) { System.out.println("44100 Sample Rate "); }
+			long sr=bytes[24]+bytes[25]<<8+bytes[26]<<16+bytes[27]<<32;
+			if(sr!=11025 && sr!=22050 && sr!=44100 && sr!=48000){
+				if(DEBUG){System.err.println("ERROR: Incorrect sample rate "+sr);}
 				return -1;
 			}
+			sampleRate[0]=sr;
 			//check BitsPerSample is 16
-			if (bytes[34] != 16 || bytes[35] != 0) {
-				if (DEBUG) {System.out.println("16 bits per sample");}
+			int t=bytes[34]+bytes[35]<<8;
+			if (t != 16 && t != 8) {
+				if (DEBUG) {System.out.println("ERROR: Incorrect bites per sample "+ t);}
 				return -1;
 			}
+			bps[0]=t;
 			//check file size
 			int subChunk2Size=bytes[36]+bytes[37]<<8;
 			if(subChunk2Size!=datasize){
@@ -149,41 +154,65 @@ public class Audio {
 
 	
 	//Extract left channel bytes
-	private  byte[] extractLeftChannels() {
-		// subtract 44 head bytes, divide by 2 leaving left channel
-		byte[] fileLeftChannel = new byte[(fileArray.length - 44) / 2];
-		// 
-		for(int i=0;i<fileLeftChannel.length/2;i++){
-			fileLeftChannel[i*2]=fileArray[44+i*4];
-			fileLeftChannel[i*2+1]=fileArray[44+i*4+1];
+	// GIVEN: nums of channel, bites per sample
+	private  byte[] extractLeftChannels(int numsOfChannel,int bps) {
+		// subtract 44 head bytes, divide by numsOfChannel leaving one channel
+		byte[] fileLeftChannel = new byte[(fileArray.length - 44) / numsOfChannel];
+		if(bps==16){
+			for(int i=0;i<fileLeftChannel.length/2;i++){
+				fileLeftChannel[i*2]=fileArray[44+i*2*numsOfChannel];
+				fileLeftChannel[i*2+1]=fileArray[44+i*2*numsOfChannel+1];
+			}
+		}else if(bps==8){
+			for(int i=0;i<fileLeftChannel.length;i++){
+				fileLeftChannel[i]=fileArray[44+i*numsOfChannel];
+			}
+		}else{
+			if(DEBUG){System.err.println("ERROR: incorrect bps in extractLeftChannels "+bps);}
+			return null;
 		}
 		return fileLeftChannel;
 	}
 	
 	//Converts byte arrays to double arrays so that they can be
 	//passed through the FFT code
-	private  double[] convertToDoubles(byte[] fileLeftChannel) {
-		double[] fileDouble;
-		ByteBuffer byteBuffer = ByteBuffer.wrap(fileLeftChannel);
-		fileDouble = new double[fileLeftChannel.length / 2];
-		int i = 0;
-		while (byteBuffer.remaining() > 2) {
-			short t = byteBuffer.getShort();
-			fileDouble[i] = t / 32768.0;
-			i++;			
+	private  double[] convertToDoubles(byte[] fileLeftChannel,int bps) {
+		double[] fileDouble=null;
+//		ByteBuffer byteBuffer = ByteBuffer.wrap(fileLeftChannel);
+		if(bps==16){
+			fileDouble = new double[fileLeftChannel.length / 2];
+			for(int i=0;i<fileDouble.length;i++){
+				short t=(short) (fileLeftChannel[i*2]+fileLeftChannel[i*2+1]<<8);
+				fileDouble[i]=t/32768.0;
+			}
+		}else if(bps==8){
+			fileDouble = new double[fileLeftChannel.length];
+			for(int i=0;i<fileDouble.length;i++){
+				fileDouble[i]=fileLeftChannel[i]/125.0;
+			}
+		}else{
+			if(DEBUG){System.err.println("ERROR: incorrect bps in convertToDoubles "+bps);}
+			return null;
 		}
 		return fileDouble;
 	}
 	// converts byte arrays to short int
-	private short[] convertToShort(byte[] fileLeftChannel){
+	private short[] convertToShort(byte[] fileLeftChannel,int bps){
 		short[] array;
-		ByteBuffer byteBuffer = ByteBuffer.wrap(fileLeftChannel);
-		array = new short[fileLeftChannel.length / 2];
-		int i = 0;
-		while (byteBuffer.remaining() > 2) {
-			short t = byteBuffer.getShort();
-			array[i] = t;
-			i++;
+		if(bps==16){
+			array = new short[fileLeftChannel.length / 2];
+			for(int i=0;i<array.length;i++){
+				short t=(short) (fileLeftChannel[i*2]+fileLeftChannel[i*2+1]<<8);
+				array[i]=t;
+			}
+		}else if(bps==8){
+			array = new short[fileLeftChannel.length];
+			for(int i=0;i<array.length;i++){
+				array[i]=fileLeftChannel[i];
+			}
+		}else{
+			if(DEBUG){System.err.println("ERROR: incorrect bps in convertToShort "+bps);}
+			return null;
 		}
 		return array;
 	}
