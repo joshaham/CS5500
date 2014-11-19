104b7684a3808ae9f0a7ae596abe36e11eaba6d8
Moses Gonzalez <mgonzalez1130@gmail.com>
2014-11-19 14:21:32 -0800

    comparator now handles milliseconds instead of seconds

--- CS5500Project/src/assignment7/Comparator.java
+++ CS5500Project/src/assignment7/Comparator.java
@@ -60,9 +60,12 @@ public class Comparator {
                 String[] song1 = firstSong.split(";");
                 String[] song2 = secondSong.split(";");
                 
+                double song1Time = Integer.parseInt(song1[1]);
+                
                 System.out.println("Match " + song1[0] + " "
-                        + song2[0] + " " + song1[1] + " " 
-                        + song2[1]);
+                        + song2[0] + " " 
+                        + Double.parseDouble(song1[1])/1000 + " " 
+                        + Double.parseDouble(song2[1])/1000);
             }
         }
 	}
