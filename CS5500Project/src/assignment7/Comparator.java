0b821b072949dd704d7c5add51fc5f2f7b9196a4
Moses Gonzalez <mgonzalez1130@gmail.com>
2014-11-19 18:55:10 -0800

    comparator modification

--- CS5500Project/src/assignment7/Comparator.java
+++ CS5500Project/src/assignment7/Comparator.java
@@ -50,10 +50,20 @@ public class Comparator {
 	}
 	
 	private void checkSong(Audio audioFile) {
+        String song1Name = audioFile.getFileName();
         HashMap<Long, String> fileHashMap = audioFile.getHashMap();
+        
+        //HashMap of songs and time segments that 
+        //returned matches with the given audioFile
+        //Key: name of song
+        //Value: double[] where double[0] is the second where the match
+        //starts in song 1, and double[1] is the second where the match
+        //starts in song 2
+        HashMap<String, double[]> matches = new HashMap<String, double[]>();
         
         for (long key : fileHashMap.keySet()) {
 //        	System.out.println(key+"  -> "+fileHashMap.get(key));
+
             if (containerHM.containsKey(key)) {
                 String firstSong = fileHashMap.get(key);
                 String secondSong = containerHM.get(key);
@@ -61,19 +71,37 @@ public class Comparator {
                 String[] song1 = firstSong.split(";");
                 String[] song2 = secondSong.split(";");
                 
-                System.out.println("Match " + song1[0] + " "
-                        + song2[0] + " " 
-                        + Double.parseDouble(song1[1])/1000 + " " 
-                        + Double.parseDouble(song2[1])/1000);
-                break;
+                double song1Start = Double.parseDouble(song1[1]);
+                double song2Start = Double.parseDouble(song2[1]);
+                double[] songMatchStarts = new double[2];
+                songMatchStarts[0] = song1Start;
+                songMatchStarts[1] = song2Start;
+                
+                if(matches.containsKey(song2[0])) {
+                    double storedSong1Start = matches.get(song2[0])[0];
+                    double storedSong2Start = matches.get(song2[0])[1];
+                    
+                    if (song1Start < storedSong1Start ||
+                            song2Start < storedSong2Start) {
+                        matches.put(song2[0], songMatchStarts);
+                    }
+                } else {
+                    matches.put(song2[0], songMatchStarts);
+                }
             }
+            
+            for (String matchKey : matches.keySet()) {
+                String song2Name = matchKey;
+                double song1Start = matches.get(matchKey)[0];
+                double song2Start = matches.get(matchKey)[1];
+                
+                System.out.println("MATCH " + song1Name + " " 
+                        + song2Name + " " 
+                        + song1Start + " "
+                        + song2Start + "\n");
+            }
+            
         }
-        
-//        System.out.println("\n\n");
-//        for(long key : containerHM.keySet()){
-//        	System.out.println(key+"  -> "+containerHM.get(key));
-//        	
-//        }
 	}
 
     private void fillHashMap(ArrayList<Audio> container) {
